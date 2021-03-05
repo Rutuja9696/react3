e54f8d770cd248a1c79a4f8b2e88e8944f123dd3
Rutuja9696 <rutuja.patil@contentstact.com>
2021-03-05 12:47:15 +0530

    removed context part

--- src/App.js
+++ src/App.js
@@ -1,20 +1,7 @@
-// import logo from './logo.svg';
-// import "./App.css";
 import { useState } from "react";
 import Hello from "./components/Hello";
-// import ThemeContext, { themes } from "./theme-context";
 function App() {
-  // const [theme, setTheme] = useState(themes.dark);
-  // const toggleTheme = () =>
-  //   theme === themes.dark ? setTheme(themes.light) : setTheme(themes.dark);
-  return (
-    <div>
-      {/* <ThemeContext.Provider value={theme}>
-        <button onClick={toggleTheme}>Change theme</button>
-        <Layout />
-      </ThemeContext.Provider> */}
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
